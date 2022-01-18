96989833f8ff52383f8ab772d874fcacc93c6cb1
Joaquin Kaplan <93784263+joaquinkaplan@users.noreply.github.com>
2022-01-18 12:37:19 -0300

    Update section8.js

--- section8.js
+++ section8.js
@@ -4,6 +4,7 @@ const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+    scrollTo(0, 0);
 
     let score = 0;
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
@@ -16,7 +17,7 @@ form.addEventListener('submit', e => {
     });
     
 //show result on page
-scrollTo(0, 0);
+
 result.querySelector('span').textContent = `${score}%`;
 result.classList.remove('d-none');
 
