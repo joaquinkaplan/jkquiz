2bd3900b1b5bd9cac4eef55bd7c98c55202af3db
Joaquin Kaplan <93784263+joaquinkaplan@users.noreply.github.com>
2022-01-24 11:15:35 -0300

    Update section8.js

--- section8.js
+++ section8.js
@@ -32,6 +32,7 @@ document.getElementById('lets-see').innerHTML = "Let's see how you did...";
 
     //check answers
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    
 
     userAnswers.forEach((answer, index) => {
         if (answer === correctAnswers[index]) {
